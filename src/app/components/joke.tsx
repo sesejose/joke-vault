1532fe875801095237adc304a7995b41253ea324
José <sesejose@gmail.com>
2025-01-17 08:22:49 +0100

    Filter and Search (Array of Terms)

--- src/app/components/joke.tsx
+++ src/app/components/joke.tsx
@@ -1,23 +1,23 @@
 export default function Joke({ text, id }) {
   return (
     <>
-      <div>
-        <li className="flex justify-between gap-x-6 py-5">
-          <div className="flex min-w-0 gap-x-4">
-            <div className="min-w-0 flex-auto">
-              <p className="text-sm/6 font-semibold text-gray-900">{text}</p>
-              <p className="mt-1 truncate text-xs/5 text-gray-500">Joke ID: {id}</p>
-            </div>
+      <li className="flex justify-between gap-x-6 py-5">
+        <div className="flex min-w-0 gap-x-4">
+          <div className="min-w-0 flex-auto">
+            <p className="text-lg font-semibold text-gray-900">{text}</p>
+            <p className="mt-1 truncate text-sm text-gray-500">ID: {id}</p>
           </div>
-          <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-            <p className="text-sm/6 text-gray-900">
-              <button>Listen</button>
-            </p>
-            <p className="mt-1 text-xs/5 text-gray-500">Lyd</p>
-          </div>
-        </li>
-        {/* This is a Joke: {text} with the id: {id} */}
-      </div>
+        </div>
+        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+          <p className="text-md text-gray-900">
+            <button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
+              Listen to it
+            </button>
+          </p>
+          {/* <p className="mt-1 text-xs/5 text-gray-500">Lyd</p> */}
+        </div>
+      </li>
+      {/* This is a Joke: {text} with the id: {id} */}
     </>
   );
 }
--- src/app/jokes/page.tsx
+++ src/app/jokes/page.tsx
@@ -23,12 +23,26 @@ interface Joke {
   joke: string;
 }
 
+interface Term {
+  term: string;
+}
+
 // ----------------------------
 
 export default function JokeFetcher() {
   const [allJokes, setAllJokes] = useState<Joke[]>([]);
   const [totalJokes, setTotalJokes] = useState<number>(0);
   const [loading, setLoading] = useState(true);
+  // To put together all the jokes that match the filter
+  const [filteredJokes, setFilteredJokes] = useState<Joke[]>([]);
+  // Filter - when selected fom the list should change the state
+  const [filter, setFilter] = useState<string>("All jokes");
+  const [shorter, setShorter] = useState<number>(0);
+  const [larger, setLarger] = useState<number>(0);
+  // Search
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  // Array of words (strings) to store the terms from the search
+  const [terms, setTerms] = useState<string[]>([]);
 
   useEffect(() => {
     setLoading(true);
@@ -53,25 +67,105 @@ export default function JokeFetcher() {
 
       await fetchOnePage(page); // starts recursion
       setAllJokes(jokes);
+      // I also set the state for filteredJokes as initial value.
+      // It state changes later with the filters.
+      setFilteredJokes(jokes);
       setLoading(false);
     })();
   }, []);
 
-  // ----------------------
+  // ---------------------- Filter --------------------------------------
+
+  // I should check what is the filter selected and see if it match with option value.
+  // If yes filter the array allJokes e.g. --> .filter() and then return / set a new setFilteredJokes(state).
+  // e.g. if the joke.text ( string ) lenght is lower that 30 return a new state
+
+  function handleFilterSelected(event) {
+    const filterSelected = event.target.value;
+    setFilter(filterSelected);
+    if (filterSelected === "Shorter") {
+      const filterShorter = allJokes.filter((joke) => joke.joke.length <= 80);
+      // console.log(filterShorter); // console to see how many - now I should display them, replace by allJokes.
+      return setFilteredJokes(filterShorter);
+    }
+    if (filterSelected === "Larger") {
+      const filterLarger = allJokes.filter((joke) => joke.joke.length >= 80);
+      // console.log(filterLarger); // console to see how many - now I should display them, replace by allJokes.
+      return setFilteredJokes(filterLarger);
+    }
+    if (filterSelected === "All jokes") {
+      return setFilteredJokes(allJokes);
+    }
+  }
+
+  // ---------------------------- Search ------------------------------------
+
+  // I define the state for the Searched Term
+  // I will use it to loop through the joke text an see if it includes that term.
+  function searchOnChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  function searchOnSubmit(event) {
+    event.preventDefault();
+    const filtered = filteredJokes.filter((joke) => {
+      const text = searchTerm.toLowerCase();
+      return joke.joke.toLowerCase().includes(text);
+    });
+    setFilteredJokes(filtered);
+    console.log(searchTerm);
+    // Once I have the term I push it to the array of terms
+    // First define a const for that array and indicate the what I do expect ( an Array of strings n this case ).
+    const arrTerms: string[] = terms;
+    // Then as I did with the jokes I push the term to that array.
+    // I have declared a const to push it to first because I can in order to
+    arrTerms.push(searchTerm);
+    console.log("Pushed Term:", arrTerms);
+    setTerms(arrTerms);
+    console.log("Terms from Search:", terms);
+  }
 
   return (
     <>
+      <div className="container">
+        <div id="search" className="">
+          <div className="">
+            <form className="form-search" action="search" onSubmit={searchOnSubmit}>
+              <label htmlFor="search-field"></label>
+              <div className="input-group flex-row-center">
+                <input id="search-field" className="input" type="text" placeholder="Search.." name="search" value={searchTerm} onChange={searchOnChange} />
+                <button className="magnify" type="submit">
+                  <span className="material-symbols-rounded">search</span>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div className="">
+          <p>There where filtered {filteredJokes.length} jokes from the general list.</p>
+          <p>There are actually {totalJokes} total jokes in this vault.</p>
+          {/* Filter Options Desktop */}
+          <div className="">Filter</div>
+          <select id="filter-desk" required="" className="filter-options" onChange={handleFilterSelected}>
+            {/* <optgroup label="Option Group" className="group"> */}
+            <option value="All jokes">All jokes</option>
+            <option value="Shorter">Shorter</option>
+            <option value="Larger">Larger</option>
+            {/* </optgroup> */}
+          </select>
+        </div>
+      </div>
       {loading ? (
         <p>Loading all jokes...</p>
       ) : (
         // <div>There are {totalJokes} totl jokes.</div>
-        <ul className="mx-10 my-10">
-          {allJokes.map((joke) => (
+        <ul className="mx-10 my-10 divide-y">
+          {filteredJokes.map((joke) => (
             <Joke key={joke.id} id={joke.id} text={joke.joke} />
           ))}
         </ul>
       )}
-      {/* <p>There are actually {totalJokes} total jokes in this vault.</p> */}
     </>
   );
 }
